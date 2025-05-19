8b616598df4d5ed43771396b165302f840f1d839
samwanyua <samexample8@gmail.com>
2025-05-20 00:15:28 +0300

    logging in python

--- Python_for_data_science/app.py
+++ Python_for_data_science/app.py
@@ -12,31 +12,31 @@ logging.basicConfig(
 
 logger = logging.getLogger("ArithmeticApp")
 
-def add(a,b):
+def add(a, b):
     result = a + b
-    logger.debug(f"Adding {a} + {b}", result)
+    logger.debug(f"Adding {a} + {b} = {result}")
     return result
 
-def subtract(a,b):
+def subtract(a, b):
     result = a - b
-    logger.debug(f"Subtracting {a} - {b}", result)
+    logger.debug(f"Subtracting {a} - {b} = {result}")
     return result
 
-def multiply(a,b):
+def multiply(a, b):
     result = a * b
-    logger.debug(f"Multiplying {a} * {b}", result)
+    logger.debug(f"Multiplying {a} * {b} = {result}")
     return result
 
-def divide(a,b):
+def divide(a, b):
     try:
         result = a / b
-        logger.debug(f"Dividing {a} * {b}", result)
+        logger.debug(f"Dividing {a} / {b} = {result}")
         return result
     except ZeroDivisionError:
         logger.error("Division by zero error")
         return None
-    
-add(12,32)
-subtract(435,3453)
-multiply(342,345)
-divide(23423,234)
+
+add(12, 32)
+subtract(435, 3453)
+multiply(342, 345)
+divide(23423, 234)

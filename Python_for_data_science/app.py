e69fb0f3ea93de079eeddc920c42af777e75135f
samwanyua <samexample8@gmail.com>
2025-05-20 00:10:14 +0300

    logging in python

---
+++ Python_for_data_science/app.py
@@ -0,0 +1,42 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    handlers=[
+        logging.FileHandler("app1.log")
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("ArithmeticApp")
+
+def add(a,b):
+    result = a + b
+    logger.debug(f"Adding {a} + {b}", result)
+    return result
+
+def subtract(a,b):
+    result = a - b
+    logger.debug(f"Subtracting {a} - {b}", result)
+    return result
+
+def multiply(a,b):
+    result = a * b
+    logger.debug(f"Multiplying {a} * {b}", result)
+    return result
+
+def divide(a,b):
+    try:
+        result = a / b
+        logger.debug(f"Dividing {a} * {b}", result)
+        return result
+    except ZeroDivisionError:
+        logger.error("Division by zero error")
+        return None
+    
+add(12,32)
+subtract(435,3453)
+multiply(342,345)
+divide(23423,234)
